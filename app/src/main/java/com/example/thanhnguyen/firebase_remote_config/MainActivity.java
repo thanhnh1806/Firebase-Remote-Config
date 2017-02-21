0ac4fcb4308f6e7ac9f8ab0dd6380b6e8188326d
Thanh Nguyen <thanhnh1806@gmail.com>
2017-02-21 10:44:27 +0700

    Create new project

---
+++ app/src/main/java/com/example/thanhnguyen/firebase_remote_config/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thanhnguyen.firebase_remote_config;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
